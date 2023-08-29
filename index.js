d374ad47eb2dad1cbdf257da6bd50a42e55d353b
daneeskripter <imdanee@seznam.cz>
2023-08-29 19:03:07 +0200

    fix

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const app = express();
 const upload = multer()
 const mongoose = require("mongoose");
 const Key = require("./models/Key");
-
+const fs = require('fs');
 mongoose.connect(process.env.MONGO_SRV, {
 }).then(() =>[
   console.log("Connected to the database!")
 ]).catch((err) =>{
     console.log('Failed connect to the database!')
 })
+
+app.use(express.static('uploads'))
 
 async function verifyKey(req, res, next) {
     const key = req.body.key;
@@ -28,6 +30,7 @@ app.post('/upload', verifyKey, upload.single('file'), function (req, res) {
     if (!req.file) {
         return res.status(400).send('No file uploaded.');
       } else {
+        fs.writeFileSync(__dirname + `/uploads/${req.file.filename}`, req.file.buffer)
         res.send(process.env.DOMAIN + `/${req.file.filename}`)
       }
 })
